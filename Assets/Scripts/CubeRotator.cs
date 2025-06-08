7c5061835d45ced0334b575f9309b7d073baff9d
Andrei Daian <andreidaian05@gmail.com>
2025-06-08 16:41:03 +0300

    Add project files.

---
+++ Assets/Scripts/CubeRotator.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CubeRotator : MonoBehaviour
+    {
+        private Vector3 _lastMousePosition;
+        public float RotationSpeed = 5f;
+
+        private void OnMouseDown()
+        {
+            _lastMousePosition = Input.mousePosition;
+        }
+
+        private void OnMouseDrag()
+        {
+            var delta = Input.mousePosition - _lastMousePosition;
+
+            var rotX = delta.y * RotationSpeed;
+            var rotY = -delta.x * RotationSpeed;
+
+            if (transform.parent != null)
+            {
+                transform.parent.Rotate(Vector3.up, rotY, Space.World);
+                transform.parent.Rotate(Vector3.right, rotX, Space.World);
+            }
+
+            _lastMousePosition = Input.mousePosition;
+        }
+    }
+}
