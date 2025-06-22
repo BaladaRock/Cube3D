cc34d8e3129d9aef7b830c73015bd78113a64025
Andrei Daian <andreidaian05@gmail.com>
2025-06-23 02:14:11 +0300

    removed comments

--- Assets/Scripts/Colorizers/CubeColorizer.cs
+++ Assets/Scripts/Colorizers/CubeColorizer.cs
@@ -30,32 +30,3 @@ namespace Assets.Scripts
         }
     }
 }
-
-
-
-//public Material FaceShaderMaterial;
-
-//public void ApplyFaceColors(GameObject piece, int x, int y, int z)
-//{
-//    var component = piece.GetComponent<MeshRenderer>();
-//    if (component == null || FaceShaderMaterial == null)
-//    {
-//        return;
-//    }
-
-//    // For interior faces, we don't apply the material
-//    var isOnOuterLayer = Mathf.Abs(x) == 1 || Mathf.Abs(y) == 1 || Mathf.Abs(z) == 1;
-//    if (!isOnOuterLayer)
-//    {
-//        Debug.Log($"Inner piece: {piece.name} at ({x},{y},{z})");
-
-//        return;
-//    }
-
-//    // Use the same material for all faces
-//    var mats = new Material[6];
-//    for (var i = 0; i < 6; i++)
-//        mats[i] = FaceShaderMaterial;
-
-//    component.materials = mats;
-//}
